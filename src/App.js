addd913498bb6b328c6380fe064fba150033fb5d
KMyroslav <kukhtarukm@gmail.com>
2021-10-24 16:52:48 +0300

    Hooks refactor

--- src/App.js
+++ src/App.js
@@ -1,83 +1,66 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import shortid from "shortid";
 import Filter from "./components/Filter/Filter";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 
-class App extends Component {
-  state = {
-    contacts: [
-      // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
-    filter: "",
-  };
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     const parsedContacts = JSON.parse(localStorage.getItem("contacts"));
     if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
+      setContacts(parsedContacts);
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log(this.state.contacts, prevState.contacts);
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+  useEffect(() => {
+    if (!contacts) {
+      return;
     }
-  }
+    localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+  }, [contacts]);
 
-  addContact = (name, number) => {
-    if (this.state.contacts.find((contact) => contact.name === name)) {
+  const addContact = (name, number) => {
+    if (contacts.find((contact) => contact.name === name)) {
       alert(`${name} is alrteady in contacts.`);
       return;
     }
-    this.setState((prevState) => {
-      return {
-        contacts: [
-          ...prevState.contacts,
-          {
-            id: shortid.generate(),
-            name: name,
-            number: number,
-          },
-        ],
-      };
-    });
+
+    setContacts((prevState) => [
+      ...prevState,
+      { id: shortid.generate(), name: name, number: number },
+    ]);
   };
 
-  handleDelete = (contactId) => {
-    this.setState({
-      contacts: this.state.contacts.reduce((acc, el) => {
+  const handleDelete = (contactId) => {
+    setContacts(
+      contacts.reduce((acc, el) => {
         if (el.id !== contactId) {
           acc.push(el);
         }
         return acc;
-      }, []),
-    });
-  };
-
-  handleFilter = (e) => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm addContact={this.addContact} />
-        <h2>Contacts</h2>
-        <Filter filter={this.state.filter} handleFilter={this.handleFilter} />
-        <ContactList
-          contacts={this.state.contacts}
-          filter={this.state.filter}
-          handleDelete={this.handleDelete}
-        />
-      </div>
+      }, [])
     );
-  }
+  };
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter
+        filter={filter}
+        handleFilter={(e) => setFilter(e.currentTarget.value)}
+      />
+      <ContactList
+        contacts={contacts}
+        filter={filter}
+        handleDelete={handleDelete}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,55 +1,51 @@
-import { Component } from "react";
+import { useState, useRef } from "react";
 import shortid from "shortid";
 import propTypes from "prop-types";
 
-class ContactForm extends Component {
-  state = {
-    name: "",
-    number: "",
-  };
+function ContactForm({ addContact }) {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const nameInputId = useRef(shortid.generate());
+  const numberInputId = useRef(shortid.generate());
 
-  handleChange = (e) => {
-    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
-  };
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.addContact(this.state.name, this.state.number);
-    this.setState({ name: "", number: "" });
+    addContact(name, number);
+    setName("");
+    setNumber("");
   };
 
-  nameInputId = shortid.generate();
-  numberInputId = shortid.generate();
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor={this.nameInputId}>Name</label>
-        <input
-          id={this.nameInputId}
-          type="text"
-          name="name"
-          value={this.state.name}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Casnumbermore d'Artagnan и т. п."
-          required
-          onChange={this.handleChange}
-        />
-        <label htmlFor={this.numberInputId}>Number</label>
-        <input
-          id={this.numberInputId}
-          type="tel"
-          name="number"
-          value={this.state.number}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-          required
-          onChange={this.handleChange}
-        />
-        <button type="submit">Add contact</button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor={nameInputId}>Name</label>
+      <input
+        id={nameInputId}
+        type="text"
+        name="name"
+        value={name}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Casnumbermore d'Artagnan и т. п."
+        required
+        onChange={(e) => {
+          setName(e.currentTarget.value);
+        }}
+      />
+      <label htmlFor={numberInputId}>Number</label>
+      <input
+        id={numberInputId}
+        type="tel"
+        name="number"
+        value={number}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+        required
+        onChange={(e) => {
+          setNumber(e.currentTarget.value);
+        }}
+      />
+      <button type="submit">Add contact</button>
+    </form>
+  );
 }
 
 ContactForm.propTypes = {
